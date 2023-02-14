4a44873e6cb56f320d00b7b86a27b5d3a835a36b
nmvrs <108933972+nmvrs@users.noreply.github.com>
2023-02-14 15:12:32 +0200

    Create x64-windows-mixed.cmake

---
+++ triplets/x64-windows-mixed.cmake
@@ -0,0 +1,12 @@
+set(VCPKG_PLATFORM_TOOLSET "v142")
+# set(VCPKG_MAX_CONCURRENCY, "1")
+
+set(VCPKG_TARGET_ARCHITECTURE x64)
+
+if(${PORT} MATCHES "onnxruntime-gpu")
+    set(VCPKG_CRT_LINKAGE dynamic)
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+else()
+	set(VCPKG_CRT_LINKAGE static)
+	set(VCPKG_LIBRARY_LINKAGE static)
+endif()
